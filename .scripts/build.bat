d1043f6b93ba22aedbf0f775a5347920a454ebec
Khazul <khazul@me.com>
2025-12-02 13:54:33 +0000

    Remnoves both zip and folder after running emm

--- .scripts/build.bat
+++ .scripts/build.bat
@@ -14,23 +14,23 @@ for /f "delims=" %%a in ('powershell -Command "(get-item '%~f0').Directory.Paren
 for /f "delims=" %%a in ('powershell -Command "$file='%~dp0\..\mod.json'; $json = (Get-Content $file -Raw) | ConvertFrom-Json; $json.version;"') do @set version=%%a
 
 @rem Prepare output folder
-rmdir /S /Q "%~dp0\..\.output" >nul 2>&1
+rmdir /S /Q "%~dp0\..\.output"
 mkdir "%~dp0\..\.output\%name%"
 
 @rem Copy necessary files to output folder
-xcopy /E /I /Y /Q "%~dp0\..\src\*" "%~dp0\..\.output\%name%\src" >nul 2>&1
-copy /Y "%~dp0\..\mod.json" "%~dp0\..\.output\%name%\mod.json" >nul 2>&1
-copy /Y "%~dp0\..\README.MD" "%~dp0\..\.output\%name%\README.MD" >nul 2>&1
+xcopy /E /I /Y /Q "%~dp0\..\src\*" "%~dp0\..\.output\%name%\src"
+copy /Y "%~dp0\..\mod.json" "%~dp0\..\.output\%name%\mod.json"
+copy /Y "%~dp0\..\README.MD" "%~dp0\..\.output\%name%\README.MD"
 
 @rem Create the zip file if 7zip is available
 @rem Get the path to 7-Zip executable
-del /F /Q "%~dp0\..\.output\%name%-%version%.zip" >nul 2>&1
+del /F /Q "%~dp0\..\.output\%name%-%version%.zip"
 for /f "delims=" %%a in ('powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Get-7ZipExePath.ps1"') do @set "sevenZipExePath=%%a"
 if "%sevenZipExePath%"=="" (
     @echo "7-Zip executable not found. Please ensure 7-Zip is installed."
 ) else (
     @rem uses 7zip as works with factorio whereas compress does not
-    "%sevenZipExePath%" a -tzip -bb0 -bso0 -bsp0 -y -aoa -o"%~dp0\..\.output" "%~dp0\..\.output\%name%-%version%.zip" "%~dp0\..\.output\%name%\*" >nul 2>&1
+    "%sevenZipExePath%" a -tzip -bb0 -bso0 -bsp0 -y -aoa -o"%~dp0\..\.output" "%~dp0\..\.output\%name%-%version%.zip" "%~dp0\..\.output\%name%\*"
     @set deployZip=true
 )
 
@@ -41,11 +41,11 @@ for /f "delims=" %%A in ('powershell -NoProfile -Command "$j = Get-Content '%~dp
 @rem Get the Steam game path
 for /f "delims=" %%a in ('powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Get-SteamGamePath.ps1" -AppID %steamAppId%') do @set "steamGamePath=%%a"
 rmdir /S /Q "%steamGamePath%\Mods\%name%"
-del /F /Q "%steamGamePath%\Mods\%name%-*.zip" >nul 2>&1
+del /F /Q "%steamGamePath%\Mods\%name%-*.zip"
 
 if "%deployZip%" == "true" (
     @rem Deploy the zip file
-    copy /Y "%~dp0\..\.output\%name%-%version%.zip" "%steamGamePath%\Mods" >nul 2>&1
+    copy /Y "%~dp0\..\.output\%name%-%version%.zip" "%steamGamePath%\Mods"
 ) else (
     @rem Deploy the unzipped mod folder
     xcopy /E /I /Y "%~dp0\..\.output\%name%" "%steamGamePath%\Mods\%name%"
@@ -64,11 +64,13 @@ if "%action%"=="export" (
     rmdir /S /Q "%emmexports%"
     mkdir "%emmexports%"
     "%emm%" run -e -g "%steamGamePath%" --export-directory "%emmexports%" --file-name "enshrouded"
-    @rem rmdir /S /Q "%steamGamePath%\Mods\%name%"
+    rmdir /S /Q "%steamGamePath%\Mods\%name%"
+    del /F /Q "%steamGamePath%\Mods\%name%-*.zip"
     goto :eof
 )
 if "%action%"=="patch" (
     "%emm%" run -p -g "%steamGamePath%" --export-directory "%emmexports%" --file-name "enshrouded"
-    @rem rmdir /S /Q "%steamGamePath%\Mods\%name%"
+    rmdir /S /Q "%steamGamePath%\Mods\%name%"
+    del /F /Q "%steamGamePath%\Mods\%name%-*.zip"
     goto :eof
 )
